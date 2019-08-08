7734e3c07ac2cf1d7e2d286c190919b0793bb8de
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-08-08 22:49:02 +0800

    create project

---
+++ src/DeepClone/CloneOperator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DeepClone
+{
+    public static class CloneOperator
+    {
+
+        public static T Clone<T>(T instance)
+        {
+
+            return CloneOperator<T>.Clone(instance);
+
+        }
+
+    }
+
+
+
+    public static class CloneOperator<T>
+    {
+
+        private readonly static Func<T, T> _func;
+        static CloneOperator() => _func = null;   //这里要对接CloneBuilder
+
+
+        public static T Clone(T instance)
+        {
+
+            return _func(instance);
+
+        }
+
+    }
+
+}
