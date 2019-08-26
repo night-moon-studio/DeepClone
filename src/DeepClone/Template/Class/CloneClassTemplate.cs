50a0bdea25247cc14c5aace17d886ad064664a87
guodf <guo_net_win@live.com>
2019-08-26 10:42:54 +0800

    添加自定义类型解析摸板

--- src/DeepClone/Template/Class/CloneClassTemplate.cs
+++ src/DeepClone/Template/Class/CloneClassTemplate.cs
@@ -1,5 +1,10 @@
 ﻿using DeepClone.Model;
+using Natasha.Operator;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 
 namespace DeepClone.Template
 {
@@ -19,16 +24,45 @@ namespace DeepClone.Template
 
         public bool MatchType(Type type)
         {
-            return type.IsClass && type != typeof(object) && type != typeof(string) && !type.IsArray;
+            return type.IsClass
+                && type != typeof(object)
+                && type != typeof(string)
+                && type != typeof(List<>)
+                && type != typeof(Dictionary<,>)
+                && !type.IsArray;
         }
-
-
-
 
         public Delegate TypeRouter(BuilderInfo info)
         {
-            //write clone code
-            return default;
+            //构造函数处理: 不存在public无参构造函数无法克隆;
+            if (info.DeclaringType.GetConstructor(new Type[0]) == null)
+            {
+                return default;
+            }
+            var builder = new StringBuilder($"new {info.DeclaringTypeName} {{");
+            foreach (var memeberInfo in info.DeclaringType.GetMembers(BindingFlags.Instance | BindingFlags.Public).Where(item=>item.MemberType==MemberTypes.Field|| item.MemberType==MemberTypes.Property))
+            {
+                if (memeberInfo.DeclaringType.IsValueType || (memeberInfo.MemberType==MemberTypes.Field &&(memeberInfo as FieldInfo).FieldType == typeof(string)))
+                {
+                    builder.Append($"{memeberInfo.Name}=oldModel.{memeberInfo.Name}");
+                    continue;
+                }
+                if (memeberInfo.DeclaringType.IsValueType || (memeberInfo.MemberType == MemberTypes.Field && (memeberInfo as PropertyInfo).PropertyType == typeof(string)))
+                {
+                    builder.Append($"{memeberInfo.Name}=oldModel.{memeberInfo.Name}");
+                    continue;
+                }
+                builder.Append($"{memeberInfo.Name}=CloneOperator.Clone(oldModel.{memeberInfo.Name})");
+            }
+
+            builder.Append("}};");
+
+            return FastMethodOperator.New
+                .OopName($"{info.DeclaringTypeName}Clone")
+                .Param(info.DeclaringType, "oldModel")
+                .OopBody(builder)
+                .Return(info.DeclaringType)
+                .Complie();
         }
 
     }
---
+++ test/DeepCloneUT/ClassTest.cs
@@ -0,0 +1,30 @@
+﻿using DeepClone;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DeepCloneUT
+{
+    public class TestModel
+    {
+        public readonly string A;
+    }
+
+    public class TestModel1
+    {
+        private TestModel1() { }
+
+        public TestModel1(string a) { }
+    }
+
+    public class ClassTest
+    {
+        [Fact]
+        public void ClassCloneTest()
+        {
+            CloneOperator.Clone(new TestModel());
+            CloneOperator.Clone(new TestModel1(""));
+        }
+    }
+}
