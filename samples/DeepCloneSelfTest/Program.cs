1aa7c79a7f3401c06c878fd29c4fda2693f04df3
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-08-26 16:09:00 +0800

    check dict

--- samples/DeepCloneSelfTest/Program.cs
+++ samples/DeepCloneSelfTest/Program.cs
@@ -6,7 +6,7 @@ namespace DeepCloneSelfTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
--- src/DeepClone/Template/Dictionary/CloneDicTemplate.cs
+++ src/DeepClone/Template/Dictionary/CloneDicTemplate.cs
@@ -8,67 +8,101 @@ using System.Text;
 
 namespace DeepClone.Template.Class
 {
-    public class CloneDicTemplate : ICloneTemplate
+    public class CloneDictTemplate : ICloneTemplate
     {
         internal readonly static int HashCode;
-        static CloneDicTemplate() => HashCode = typeof(CloneDicTemplate).GetHashCode();
+        static CloneDictTemplate() => HashCode = typeof(CloneDictTemplate).GetHashCode();
+
+
+
 
         public override int GetHashCode() => HashCode;
 
+
+
+
         public bool MatchType(Type type)
         {
-            return type is IDictionary;
+
+            return type.IsImplementFrom<IDictionary>();
+
         }
+
+
+
 
         public Delegate TypeRouter(Model.BuilderInfo info)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($@"{info.DeclaringType.GetDevelopName()} newDic = new {info.DeclaringType.GetDevelopName()}(oldDic.Count);");
-            if (info.DeclaringType.IsGenericType)
+
+            StringBuilder scriptBuilder = new StringBuilder();
+            scriptBuilder.AppendLine(@"if(old!=default){ return ");
+
+
+            //初始化目标，区分实体类和接口
+            if (!info.DeclaringType.IsInterface)
             {
-                sb.AppendLine($@"foreach(KeyValuePair<{info.DeclaringType.GetGenericArguments()[0].GetDevelopName()},{info.DeclaringType.GetGenericArguments()[1].GetDevelopName()}> item in oldDic) {{");
-                // 复制值
-                if (info.DeclaringType.GetGenericArguments()[0].IsOnceType() && info.DeclaringType.GetGenericArguments()[1].IsOnceType())
-                {
-                    sb.AppendLine($@"newDic.Add(item.Key,item.Value);");
-                }
-                else if (info.DeclaringType.GetGenericArguments()[0].IsOnceType() && !info.DeclaringType.GetGenericArguments()[1].IsOnceType())
-                {
-                    sb.AppendLine($@" var value =CloneOperator.Clone(item.Value);");
-                    sb.AppendLine($@"newDic.Add(item.Key,value);");
-                }
-                else if (!info.DeclaringType.GetGenericArguments()[0].IsOnceType() && info.DeclaringType.GetGenericArguments()[1].IsOnceType())
-                {
-                    sb.AppendLine($@" var key =CloneOperator.Clone(item.Key);");
-                    sb.AppendLine($@"newDic.Add(key,item.Value);");
-                }
-                else if (!info.DeclaringType.GetGenericArguments()[0].IsOnceType() && !info.DeclaringType.GetGenericArguments()[1].IsOnceType())
-                {
-                    sb.AppendLine($@" var key =CloneOperator.Clone(item.Key);");
-                    sb.AppendLine($@" var value =CloneOperator.Clone(item.Value);");
-                    sb.AppendLine($@"newDic.Add(key,value);");
-                }
+
+                scriptBuilder.AppendLine($"new {info.DeclaringTypeName}");
+
+            }
+           scriptBuilder.AppendLine(" (old.Select(item=>{{return  new KeyValuePair(");
+
+
+            //克隆Key
+            var keyType = info.DeclaringType.GetGenericArguments()[0];
+            if (keyType.IsValueType 
+                || keyType == typeof(string) 
+                || keyType == typeof(Delegate)
+                || keyType == typeof(MulticastDelegate))
+            {
+
+                scriptBuilder.Append($"item.Key,");
+
             }
             else
             {
-                //sb.AppendLine($@"foreach(DictionaryEntry item in oldDic) {{");
-                throw new Exception("暂不支持非泛型字典的拷贝");
+
+                scriptBuilder.AppendLine($"CloneOperator.Clone(item.Key),");
+
             }
-            sb.AppendLine(@"}");
-            sb.AppendLine(@"return newDic;");
+
+
+            //克隆Value
+            var valueType = info.DeclaringType.GetGenericArguments()[1];
+            if (valueType.IsValueType
+                || valueType == typeof(string)
+                || valueType == typeof(Delegate)
+                || valueType == typeof(MulticastDelegate))
+            {
+
+                scriptBuilder.Append($"item.Value");
+
+            }
+            else
+            {
+
+                scriptBuilder.AppendLine($"CloneOperator.Clone(item.Value)");
+
+            }
+
+
+            //补全括号，返回默认值。
+            scriptBuilder.AppendLine(");}));}return default;");
+
 
             var action = FastMethodOperator.New
                             .Using("DeepClone")
-                            .Using("Natasha")
+                            .Using("System.Linq")
                             .Using(typeof(IDictionary))
-                            .OopName($"DeepClone{Guid.NewGuid().ToString().Replace("-", string.Empty)}")
-                            .MethodName("Clone")
-                            .Param(info.DeclaringType, "oldDic")
-                            .MethodBody(sb.ToString())
+                            .Using(typeof(KeyValuePair<,>))
+                            .Param(info.DeclaringType, "old")
+                            .MethodBody(scriptBuilder.ToString())
                             .Return(info.DeclaringType)
                             .Complie();
-
             return action;
+
         }
+
     }
+
 }
