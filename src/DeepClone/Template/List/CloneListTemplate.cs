36e9fce8f15fa10876302c5996924fbcce3b3945
wxn401 <529455823@qq.com>
2019-09-01 01:35:45 +0800

    Signed-off-by: List

--- src/DeepClone/Template/List/CloneListTemplate.cs
+++ src/DeepClone/Template/List/CloneListTemplate.cs
@@ -9,7 +9,7 @@ using System.Text;
 
 namespace DeepClone.Template.List
 {
-    public class CloneListTemplate
+    public class CloneListTemplate : ICloneTemplate
     {
         internal readonly static int HashCode;
         static CloneListTemplate() => HashCode = typeof(CloneListTemplate).GetHashCode();
@@ -18,7 +18,7 @@ namespace DeepClone.Template.List
 
         public bool MatchType(Type type)
         {
-            return type is IList  && type != typeof(object) && type != typeof(string) ;
+            return type.IsGenericType && type != typeof(object) && type != typeof(string);
         }
 
         public Delegate TypeRouter(Model.BuilderInfo info)
@@ -27,9 +27,9 @@ namespace DeepClone.Template.List
             sb.AppendLine($@"{info.DeclaringType.GetDevelopName()} newlist = new {info.DeclaringType.GetDevelopName()}(oldlist.Count);");
             if (info.DeclaringType.IsGenericType)
             {
-                sb.AppendLine($@"foreach ({info.DeclaringType.GetGenericArguments()[0].GetDevelopName()} li in oldlist)");
+                sb.AppendLine($@"foreach ({info.DeclaringType.GetGenericArguments()[0].GetDevelopName()} li in oldlist){{");
                 // 值类型
-                if (info.DeclaringType.IsOnceType())
+                if (info.ArrayBaseType.IsOnceType())
                 {
                     sb.Append("newlist.Add(li);");
                 }
@@ -44,21 +44,21 @@ namespace DeepClone.Template.List
                 throw new Exception("暂不支持");
             }
             sb.AppendLine(@"}");
-            sb.AppendLine(@"return newList;");
+            sb.AppendLine(@"return newlist;");
 
             var action = FastMethodOperator.New
                             .Using("DeepClone")
                             .Using("Natasha")
-                            .Using(info.DeclaringType.GetGenericArguments()[0].GetDevelopName())// List 中的类型
+                            .Using(typeof(Array))
                             .Using(typeof(IList))
-                            //.OopName($"DeepClone{Guid.NewGuid().ToString().Replace("-", string.Empty)}")
-                            //.MethodName("Clone")
+                            .OopName($"DeepClone{Guid.NewGuid().ToString().Replace("-", string.Empty)}")
+                            .MethodName("Clone")
                             .Param(info.DeclaringType, "oldlist")
                             .MethodBody(sb.ToString())
                             .Return(info.DeclaringType)
                             .Complie();
             return action;
-            
+
         }
     }
 }
