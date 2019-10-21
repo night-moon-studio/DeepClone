90d1dd6d758d3bff115e144573cbf1f333ae5f19
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-10-21 14:17:07 +0800

    Revert "添加object克隆"
    This reverts commit 2cc7af17f4d6b4fe3e1007598cd684a5a2f341ba.

--- src/DeepClone/FastCloneOperator.cs
+++ src/DeepClone/FastCloneOperator.cs
@@ -1,6 +1,4 @@
 ﻿using DeepClone.Builder;
-using Natasha;
-using Natasha.Operator;
 using System;
 
 namespace DeepClone
@@ -10,22 +8,11 @@ namespace DeepClone
 
         public static T Clone<T>(T instance)
         {
-            if (instance == null)
-            {
-                return instance;
-            }
 
-            if (typeof(T) == typeof(object))
-            {
-                var func=FastObjectCloneBuilder.Create(instance.GetType());
-                return (T)func(instance);
-            }
-            if (typeof(T).IsSimpleType()) 
-            {
-                return instance;
-            }
             return FastCloneOperator<T>.Clone(instance);
+
         }
+
     }
 
 
