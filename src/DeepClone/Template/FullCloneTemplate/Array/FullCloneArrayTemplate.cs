7347eaed3e1cf03cee34f8b0c8056dcc6c503a25
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-10-28 22:47:34 +0800

    23341231231234

--- src/DeepClone/Template/FullCloneTemplate/Array/FullCloneArrayTemplate.cs
+++ src/DeepClone/Template/FullCloneTemplate/Array/FullCloneArrayTemplate.cs
@@ -19,26 +19,52 @@ namespace DeepClone.Template
 
         public Delegate TypeRouter(NBuildInfo info)
         {
-            string methodBody = null;
+
+            string methodBody;
 
             // 简单类型
             // SimpleType
             if (info.ArrayBaseType.IsSimpleType())
             {
+
                 methodBody = GenerateSimpleTypeClone(info);
+
             }
-            // 多维+复杂
-            // MultiD+Complex
-            else if (info.ArrayDimensions > 1)
+            else if (info.ArrayBaseType == typeof(object))
             {
-                methodBody = GenerateMultiDWithComplexClone(info);
+
+                // 多维+复杂
+                // MultiD+Complex
+                if (info.ArrayDimensions > 1)
+                {
+                    methodBody = GenerateMultiDWithObjectClone(info);
+                }
+                // 1维+复杂 及 锯齿+复杂
+                // 1D+Complex and Jagged+Complex
+                else
+                {
+                    methodBody = GenerateJaggedWithObjectClone2(info);
+                }
+
             }
-            // 1维+复杂 及 锯齿+复杂
-            // 1D+Complex and Jagged+Complex
             else
             {
-                methodBody = GenerateJaggedWithComplexClone2(info);
+
+                // 多维+复杂
+                // MultiD+Complex
+                if (info.ArrayDimensions > 1)
+                {
+                    methodBody = GenerateMultiDWithComplexClone(info);
+                }
+                // 1维+复杂 及 锯齿+复杂
+                // 1D+Complex and Jagged+Complex
+                else
+                {
+                    methodBody = GenerateJaggedWithComplexClone2(info);
+                }
+
             }
+
 
             var action = FastMethodOperator.New
                             .Using("DeepClone")
@@ -50,6 +76,10 @@ namespace DeepClone.Template
 
             return action;
         }
+
+
+
+
 
         /// <summary>
         /// 1维+复杂 及 锯齿+复杂
@@ -72,6 +102,35 @@ namespace DeepClone.Template
                 ";
             return methodBody;
         }
+
+
+
+
+        /// <summary>
+        /// 1维+复杂 及 锯齿+复杂
+        /// 1D+Complex 及 Jagged+Complex
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        private string GenerateJaggedWithObjectClone2(NBuildInfo info)
+        {
+            var methodBody = $@"
+                    if(oldIns==default) return default;
+                    {info.CurrentTypeName} newIns = 
+                        ({info.CurrentTypeName})Array.CreateInstance(
+                                                        typeof({info.ElementTypeName})
+                                                        , oldIns.Length
+                                                        );
+                    for (int i = 0; i < newIns.Length; i++)
+                        newIns[i] = FullObjectCloneOperator.Clone(oldIns[i]);
+                    return newIns;
+                ";
+            return methodBody;
+        }
+
+
+
+
 
         /// <summary>
         /// 多维+复杂
@@ -105,6 +164,45 @@ namespace DeepClone.Template
             ";
             return methodBody;
         }
+
+
+
+
+        /// <summary>
+        /// 多维+复杂
+        /// MultiD+Complex
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        private string GenerateMultiDWithObjectClone(NBuildInfo info)
+        {
+            var sb = new StringBuilder();
+            var varNameArr = new string[info.ArrayDimensions];
+            var multiArrLenArr = new string[info.ArrayDimensions];
+
+            for (int i = 0; i < info.ArrayDimensions; i++)
+            {
+                var varName = $"_{i}";
+                varNameArr[i] = varName;
+                multiArrLenArr[i] = $"oldIns.GetLength({i})";
+
+                sb.AppendLine($"for (int {varName} = 0; {varName} < oldIns.GetLength({i}); {varName}++)");
+            }
+            var varNameStr = string.Join(",", varNameArr);
+            var multiArrTypeStr = string.Join(",", multiArrLenArr);
+
+            var methodBody = $@"
+                if(oldIns==default) return default;
+                {info.CurrentTypeName} newIns = new {info.ElementTypeName}[{multiArrTypeStr}];
+                {sb.ToString()}
+                    newIns[{varNameStr}] = FullObjectCloneOperator.Clone(oldIns[{varNameStr}]);
+                return newIns;
+            ";
+            return methodBody;
+        }
+
+
+
 
         /// <summary>
         /// 简单类型
